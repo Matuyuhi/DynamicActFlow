e9d7fc96959b4ad2e95b98ecbdebe1969d224103
yuhi yamane <bird9.yuhi@gmail.com>
2024-06-12 15:40:06 +0900

    fix

--- Assets/DynamicActFlow/update-version.sh
+++ Assets/DynamicActFlow/update-version.sh
@@ -6,6 +6,7 @@ VERSION_TYPE=$1
 # package.json を更新
 npm version $VERSION_TYPE --no-git-tag-version
 
+NEW_VERSION=$(node -p "require('./package.json').version")
 # Git でコミットとタグを追加
 git add package.json
 git commit -m "Release version ${NEW_VERSION}"
