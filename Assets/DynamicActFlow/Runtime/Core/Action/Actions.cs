1f525ec31ae4312796f963c2d819bca76a846bb3
yuhi yamane <bird9.yuhi@gmail.com>
2024-08-30 22:39:04 +0900

    :recycle: refactor: Use System and deprecate IfEnd method
    Imported System namespace and marked the IfEnd method with Obsolete attribute. Added new IfEnd methods for multiple triggers.

--- Assets/DynamicActFlow/Runtime/Core/Action/Actions.cs
+++ Assets/DynamicActFlow/Runtime/Core/Action/Actions.cs
@@ -1,5 +1,6 @@
 #region
 
+using System;
 using DynamicActFlow.Runtime.Core.Flow;
 
 #endregion
@@ -58,6 +59,7 @@ namespace DynamicActFlow.Runtime.Core.Action
         /// <param name="triggers">The triggers to check.</param>
         /// <returns>The updated action reference.</returns>
         /// <exception cref="NotSupportedParameterException"></exception>
+        [Obsolete("Use IfEnd instead")]
         public static ActionRef IfEnd(this ActionRef action, TriggerBase[] triggers)
         {
             if (action.Type() == ActionType.Called)
@@ -68,6 +70,65 @@ namespace DynamicActFlow.Runtime.Core.Action
             action.SetTriggers(triggers);
             return action;
         }
+
+        /// <summary>
+        ///     Ends the conditional if statements.
+        /// </summary>
+        /// <param name="action">The action reference.</param>
+        /// <param name="trigger1">The trigger to check.</param>
+        /// <param name="trigger2">The trigger to check.</param>
+        /// <returns>The updated action reference.</returns>
+        public static ActionRef IfEnd(this ActionRef action, TriggerBase trigger1, TriggerBase trigger2)
+        {
+            if (action.Type() == ActionType.Called)
+            {
+                throw new NotSupportedParameterException("IfEnd is not supported for Called actions");
+            }
+
+            action.SetTriggers(new[] {trigger1, trigger2});
+            return action;
+        }
+        
+        /// <summary>
+        ///     Ends the conditional if statements.
+        /// </summary>
+        /// <param name="action">The action reference.</param>
+        /// <param name="trigger1">The trigger to check.</param>
+        /// <param name="trigger2">The trigger to check.</param>
+        /// <param name="trigger3">The trigger to check.</param>
+        /// <returns>The updated action reference.</returns>
+        public static ActionRef IfEnd(this ActionRef action, TriggerBase trigger1, TriggerBase trigger2, TriggerBase trigger3)
+        {
+            if (action.Type() == ActionType.Called)
+            {
+                throw new NotSupportedParameterException("IfEnd is not supported for Called actions");
+            }
+
+            action.SetTriggers(new[] {trigger1, trigger2, trigger3});
+            return action;
+        }
+        
+                
+        /// <summary>
+        ///     Ends the conditional if statements.
+        /// </summary>
+        /// <param name="action">The action reference.</param>
+        /// <param name="trigger1">The trigger to check.</param>
+        /// <param name="trigger2">The trigger to check.</param>
+        /// <param name="trigger3">The trigger to check.</param>
+        /// <param name="trigger4">The trigger to check.</param>
+        /// <returns>The updated action reference.</returns>
+        public static ActionRef IfEnd(this ActionRef action, TriggerBase trigger1, TriggerBase trigger2, TriggerBase trigger3, TriggerBase trigger4)
+        {
+            if (action.Type() == ActionType.Called)
+            {
+                throw new NotSupportedParameterException("IfEnd is not supported for Called actions");
+            }
+
+            action.SetTriggers(new[] {trigger1, trigger2, trigger3, trigger4});
+            return action;
+        }
+
 
         /// <summary>
         ///     Creates a new instance of TriggerRef for the specified tag.
